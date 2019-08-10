2664cca3d6d52fa2a0d3444309c4394369d93f0c
Leila <nb.leila10@gmail.com>
2019-08-09 20:02:33 -0500

    Reschedule message if updated

--- reminder/reminder-router.js
+++ reminder/reminder-router.js
@@ -42,15 +42,7 @@ router.post("/", restrict, async (req, res) => {
       const reminderId = await Reminder.add(reminder);
       const newReminder = await Reminder.getById(reminderId, userId);
 
-      const { recipientName, recipientEmail, messageText } = reminder;
-      let date = reminder.date;
-      // if (typeof date === "number" || typeof date === "string") {
-      //   date = new Date(date);
-      // }
-      // const year = date.getUTCFullYear();
-      // const month = date.getUTCMonth();
-      // const day = date.getUTCDate();
-      // const sendDate = new Date(year, month, day, 11, 10, 00);
+      const { recipientName, recipientEmail, messageText, date } = reminder;
       try {
         const user = await Users.findBy({ id: userId }).first();
         if (!user.name) user.name = "Anonymous";
@@ -77,11 +69,11 @@ router.post("/", restrict, async (req, res) => {
                 {
                   type: "text/html",
                   value: `
-                    <div style="background-color:lightgrey">
+                    <div>
                       <h1 style="color:#4c688f;font-size:30px;">Hello ${recipientName}</h1>
-                      <h2>from ${user.name} - ${user.email}</h2>
+                      <h3>from ${user.name} - ${user.email}</h3>
                       <p>${messageText}</p>
-                      <p>Sent from <span style="color:#f3eec3;font-size:30px;">Forget Me Not</span></p>
+                      <p>Sent from <span style="color:#284243;font-size:30px;">ForgetMeNot</span></p>
                     </div>
                     `
                 }
@@ -155,82 +147,76 @@ router.put("/:id", restrict, async (req, res) => {
           count: count,
           message: "Please provide a valid reminder id and information"
         });
-      }
-      // } else {
-      //   //update scheduler to send the email with the updated date
-      //   if (reminderInfo.sendDate) {
-      //     // get the scheduler
-      //     const scheduled = scheduler.scheduledJobs[reminderId.toString()];
-      //     if (scheduled) {
-      //       //cancel the previously scheduled email
-      //       scheduled.cancel();
-      //     }
-      //     //create a new one with the new date
-      //     let date = reminderInfo.sendDate;
-      //     if (typeof date === "number" || typeof date === "string") {
-      //       date = new Date(date);
-      //     }
-      //     const year = date.getUTCFullYear();
-      //     const month = date.getUTCMonth();
-      //     const day = date.getUTCDate();
-      //     const sendDate = new Date(year, month, day, 11, 10, 00);
-      //     try {
-      //       const user = await Users.findBy({ id: userId }).first();
-      //       if (!user.name) user.name = "friend";
-      //       if (!user.email) user.email = "no-email";
-      //       const reminder = await Reminder.getById(reminderId, userId);
-      //       const { recipientEmail, recipientName, message } = reminder;
-      //       scheduler.scheduleJob(reminderId.toString(), sendDate, async function() {
-      //         console.log(`Schedule for date: ${sendDate}`);
-      //         const request = sg.emptyRequest({
-      //           method: "POST",
-      //           path: "/v3/mail/send",
-      //           body: {
-      //             personalizations: [
-      //               {
-      //                 to: [
-      //                   {
-      //                     email: recipientEmail
-      //                   }
-      //                 ],
-      //                 subject: "You have a message from someone who cares!"
-      //               }
-      //             ],
-      //             from: {
-      //               email: "no-reply@no-reply.com"
-      //             },
-      //             content: [
-      //               {
-      //                 type: "text/html",
-      //                 value: `
-      //               <h1>Hello ${recipientName}</h1>
-      //               <h2>from ${user.name} - ${user.email}</h2>
-      //               <p>${message}</p>`
-      //               }
-      //             ]
-      //           }
-      //         });
-      //         sg.API(request)
-      //           .then(response => {
-      //             console.log(response.statusCode);
-      //             console.log(response.body);
-      //             console.log(response.headers);
-      //           })
-      //           .catch(error => {
-      //             console.log(error.response.statusCode);
-      //           });
+      } else {
+        //update scheduler to send the email with the updated date
+        if (reminderInfo.date) {
+          // get the scheduler
+          const scheduled = scheduler.scheduledJobs[reminderId.toString()];
+          if (scheduled) {
+            //cancel the previously scheduled email
+            scheduled.cancel();
+          }
+          try {
+            const user = await Users.findBy({ id: userId }).first();
+            if (!user.name) user.name = "Anonymous";
+            if (!user.email) user.email = "no-email";
+            const reminder = await Reminder.getById(reminderId, userId);
+            const { recipientEmail, recipientName, messageText, date } = reminder;
+            scheduler.scheduleJob(reminderId.toString(), date, async function() {
+              const request = sg.emptyRequest({
+                method: "POST",
+                path: "/v3/mail/send",
+                body: {
+                  personalizations: [
+                    {
+                      to: [
+                        {
+                          email: recipientEmail
+                        }
+                      ],
+                      subject: "You have a message from someone who cares!"
+                    }
+                  ],
+                  from: {
+                    email: "no-reply@no-reply.com"
+                  },
+                  content: [
+                    {
+                      type: "text/html",
+                      value: `
+                        <div>
+                          <h1 style="color:#4c688f;font-size:30px;">Hello ${recipientName}</h1>
+                          <h3>from ${user.name} - ${user.email}</h3>
+                          <p>${messageText}</p>
+                          <p>Sent from <span style="color:#284243;font-size:30px;">ForgetMeNot</span></p>
+                        </div>
+                      `
+                    }
+                  ]
+                }
+              });
+              sg.API(request)
+                .then(response => {
+                  console.log(response.statusCode);
+                  console.log(response.body);
+                  console.log(response.headers);
+                })
+                .catch(error => {
+                  console.log(error.response.statusCode);
+                });
 
-      //         try {
-      //           await Reminder.update(reminderId, userId, { sent: true });
-      //         } catch (err) {
-      //           console.log(err);
-      //         }
-      //       });
-      //     } catch (err) {
-      //       console.log("ERROR: ", err);
-      //     }
-      //   }
-      res.status(200).json({ count: count, message: "The reminder has been updated" });
+              try {
+                await Reminder.update(reminderId, userId, { sent: true });
+              } catch (err) {
+                console.log(err);
+              }
+            });
+          } catch (err) {
+            console.log("ERROR: ", err);
+          }
+        }
+        res.status(200).json({ count: count, message: "The reminder has been updated" });
+      }
     } catch (err) {
       res.status(500).json({ errorMessage: "There was an error removing the reminder from the database" });
     }
